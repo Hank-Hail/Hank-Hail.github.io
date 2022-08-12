30aab32314653b14b3fed5774229f3efeaa5cd6b
Hank Hail <102634561+Hank-Hail@users.noreply.github.com>
2022-08-12 18:06:58 +0000

    t

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://northshorehigh.com/',
+    bare: 'https://northshorehigh.com',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
